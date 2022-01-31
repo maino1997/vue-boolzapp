0068da1e02f0bf325b97b7e1fac248ac06048d0f
Sasha Mainardi <sashamainardi4@gmail.com>
2022-01-31 16:10:51 +0100

    first milestone logic

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log('ok', Vue);
+
+Vue.config.devtools = true;
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        user: {
+            name: 'Sasha Mainardi',
+            avatar: '_io'
+        },
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+            },
+        ]
+    },
+});
